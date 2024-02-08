af136a1b9cc425afece053bc3f5e4309d5d347fe
Kirti Sharma <kirti04696@gmail.com>
2024-02-09 01:13:58 +0530

    added product details page and login required js

--- js/index.js
+++ js/index.js
@@ -102,3 +102,106 @@ async function login2() {
     console.log("something went wrong");
   }
 }
+
+function createProductElement(data) {
+  // let data = {
+  //   productId: 1,
+  //   productName: "Rice",
+  //   productVarieties: [
+  //     {
+  //       productVarietyId: 1,
+  //       unit: 1,
+  //       price: 500,
+  //       images: [
+  //         {
+  //           productImgId: 1,
+  //           imageUrl:
+  //             "supermarket-food-gift-baskets-grocery-store-shopping-cart-fresh-food-picnic-baskets-hamper-ketogenic-diet-png-clipart.jpg",
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // };
+
+  let image = document.createElement("img");
+
+  let title = document.createElement("h5");
+  let price = document.createElement("p");
+  let addcartBtn = document.createElement("a");
+  let carBody = document.createElement("div");
+  let card = document.createElement("div");
+
+  image.classList.add("card-img-top");
+  image.setAttribute(
+    "src",
+    "supermarket-food-gift-baskets-grocery-store-shopping-cart-fresh-food-picnic-baskets-hamper-ketogenic-diet-png-clipart.jpg"
+  );
+
+  title.classList.add("card-title");
+  title.innerHTML = data.productName;
+
+  price.classList.add("card-text");
+  price.innerHTML = data.productVarieties[0].price;
+
+  addcartBtn.classList.add("btn");
+  addcartBtn.classList.add("btn-primary");
+  addcartBtn.setAttribute("href", "###");
+  addcartBtn.innerHTML = "Add to cart";
+
+  carBody.classList.add("card-body");
+
+  card.classList.add("card");
+  card.classList.add("m-4");
+  card.setAttribute("style", "width: 15rem");
+
+  carBody.appendChild(title);
+  carBody.appendChild(price);
+  carBody.appendChild(addcartBtn);
+
+  card.appendChild(image);
+  card.appendChild(carBody);
+  return card;
+}
+
+async function loadProductData() {
+  let response = await fetch("http://localhost:8080/product/");
+  let productData = await response.json();
+  let product = document.getElementById("product-container");
+  for (let i = 0; i < productData.data.length; i++) {
+    product.appendChild(createProductElement(productData.data[i]));
+  }
+}
+
+// -----------------------product details.html-----------------------
+
+async function loadProductDetailById(productId) {
+  let response = await fetch("http://localhost:8080/product/" + productId);
+  let productData = await response.json();
+  let data = productData.data;
+  console.log(data);
+  document.getElementById("product-title").innerHTML = data.productName;
+  document.getElementById("discription").innerHTML =
+    data.productDetail.productDescription;
+  document.getElementById("price").innerHTML =
+    "Price &#x20B9;" + data.productVarieties[0].price;
+  document
+    .getElementById("product-img")
+    .setAttribute("src", data.productVarieties[0].images[0].imageUrl);
+
+  let descriptionTag = document.getElementById("product-details");
+
+  for (const [key, value] of Object.entries(data.productDetail)) {
+    if (key == "productDetailsId" || key == "productDescription") {
+      continue;
+    }
+    if (value != null) {
+      let h5 = document.createElement("h5");
+      h5.innerHTML = key;
+      let content = document.createElement("p");
+      content.innerHTML = value;
+
+      descriptionTag.appendChild(h5);
+      descriptionTag.appendChild(content);
+    }
+  }
+}
